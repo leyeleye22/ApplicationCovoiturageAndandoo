cdef9face2b3825c441e4962e6ac88fcabb8329b
leyeleye22 <146863959+leyeleye22@users.noreply.github.com>
2024-02-02 13:40:22 +0000

    Gestion es exception

--- andandoo/database/migrations/2024_01_13_132535_create_avis_table.php
+++ andandoo/database/migrations/2024_01_13_132535_create_avis_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string('Contenue');
             $table->integer('Notation');
             $table->foreignId('utilisateur_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
-            $table->foreignId('voiture_id')->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->timestamps();
         });
     }
