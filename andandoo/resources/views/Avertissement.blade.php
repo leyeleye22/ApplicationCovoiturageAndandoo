8d53fcc319754618001c53c5c09830fbfcece7df
leyeleye22 <146863959+leyeleye22@users.noreply.github.com>
2024-02-10 16:45:45 +0000

    creation

--- andandoo/resources/views/Avertissement.blade.php
+++ andandoo/resources/views/Avertissement.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <title>Avertissement</title>
     <style>
         .card {
@@ -36,10 +36,11 @@
             border-radius: 9999px;
         }
 
-        .image svg {
+        .image i {
             color: #DC2626;
             width: 1.5rem;
             height: 1.5rem;
+            text-align: center;
         }
 
         .content {
@@ -68,11 +69,7 @@
     <div class="card">
         <div class="header">
             <div class="image">
-                <svg aria-hidden="true" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" fill="none">
-                    <path
-                        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
-                        stroke-linejoin="round" stroke-linecap="round"></path>
-                </svg>
+                <i class="bi bi-exclamation-triangle"></i>
             </div>
             <div class="content">
                 <span class="title">Avertissement</span>
